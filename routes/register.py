f8036b2b24489d6903f798552839ef0b41da7670
Krystian Żywczak (DUP3RELE) <97246430+DUP3RELE@users.noreply.github.com>
2024-03-14 12:15:19 +0100

    Update register.py

--- routes/register.py
+++ routes/register.py
@@ -7,22 +7,18 @@ register_bp = Blueprint('register_bp', __name__, url_prefix='/api')
 
 @register_bp.route('/register', methods=['POST'])
 def register():
-    # Odbierz dane z żądania
     data = request.json
     username = data.get('username')
     email = data.get('email')
     password = data.get('password')
 
-    # Sprawdź, czy użytkownik już istnieje
     if User.query.filter_by(email=email).first():
         return jsonify({'message': 'Email is already in use'}), 409
 
-    # Utwórz nowego użytkownika i haszuj jego hasło
     new_user = User(username=username, email=email)
     new_user.password_hash = generate_password_hash(password)
 
-    # Dodaj użytkownika do bazy danych
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({'message': 'User has been registered successfully'}), 201
+    return jsonify({'message': 'uzytkownik zarejestrowany poprawnie'}), 201
