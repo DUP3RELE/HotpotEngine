07166048dce4863e43cd6bbf8f5a406134099287
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-04-09 15:44:04 +0200

    working on employee acc

--- models/restaurants.py
+++ models/restaurants.py
@@ -8,6 +8,7 @@ class Restaurant(db.Model):
     restaurantname = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    # employee = db.relationship('Employee', backref='restaurant', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -15,4 +16,3 @@ class Restaurant(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
-    employees = db.relationship('Employee', backref='restaurant', lazy='dynamic')
