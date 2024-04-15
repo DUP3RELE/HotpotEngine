9c62dbb9deba84c635a58a04af3b28d43842dba2
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-04-15 17:32:11 +0200

    fetching employees

--- routes/create_employee.py
+++ routes/create_employee.py
@@ -5,32 +5,18 @@ from flask_jwt_extended import jwt_required
 
 employee_bp = Blueprint('employee_bp', __name__, url_prefix='/api')
 
-@employee_bp.route('/create_employee', methods=['POST'])
+
+@employee_bp.route('/get_employees', methods=['GET'])
 @jwt_required()
-def create_employee():
-    data = request.get_json()
-    restaurant_id = data.get('restaurant_id')
-    login = data.get('login')
-    password = data.get('password')
-    name = data.get('name')
-    position = data.get('position')
-
+def get_employees():
+    restaurant_id = request.args.get('restaurant_id')
     if not restaurant_id:
-        return jsonify({'message': 'Brak restaurant_id'}), 400
+        return jsonify({'message': 'Brak ID restauracji w żądaniu'}), 400
 
-    existing_employee = Employee.query.filter_by(login=login).first()
-    if existing_employee:
-        return jsonify({'message': 'Pracownik o takim loginie już istnieje'}), 400
+    employees = Employee.query.filter_by(restaurant_id=restaurant_id).all()
+    employees_list = [
+        {"id": emp.employee_id, "name": emp.name, "position": emp.position}
+        for emp in employees
+    ]
 
-    new_employee = Employee(
-        restaurant_id=restaurant_id,
-        login=login,
-        name=name,
-        position=position
-    )
-    new_employee.password = password
-
-    db.session.add(new_employee)
-    db.session.commit()
-
-    return jsonify({'message': 'Pracownik został pomyślnie utworzony'}), 201
+    return jsonify(employees_list), 200
