09d555ea3b4052ddf4faaaa733daae1f217f60ea
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-06-11 16:54:45 +0200

    added function for employee login

--- routes/login.py
+++ routes/login.py
@@ -1,35 +1,38 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash
 from models.restaurants import Restaurant
+from models.employees import Employee  # import modelu Employee
 from flask_jwt_extended import create_access_token
 from datetime import timedelta
 
 login_bp = Blueprint('login_bp', __name__, url_prefix='/api')
 
-
 @login_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
 
-    email = data.get('email')
+    identifier = data.get('identifier')
     password = data.get('password')
 
-    if not email or not password:
-        return jsonify({'message': 'Wpisz email i hasło'}), 400
+    if not identifier or not password:
+        return jsonify({'message': 'Wpisz email/login i hasło'}), 400
 
-    restaurant = Restaurant.query.filter_by(email=data.get('email')).first()
-
-    if restaurant is None:
-        return jsonify({'message': 'Nieprawidłowy adres E-mail'}), 401
-
-    if not check_password_hash(restaurant.password_hash, password):
-        return jsonify({'message': 'Niepoprawne hasło'}), 401
-
-    if restaurant and check_password_hash(restaurant.password_hash, data.get('password')):
+    # Sprawdź czy użytkownik to Restaurant
+    restaurant = Restaurant.query.filter_by(email=identifier).first()
+    if restaurant and check_password_hash(restaurant.password_hash, password):
         additional_claims = {"restaurant_id": restaurant.id}
         access_token = create_access_token(identity=restaurant.id, additional_claims=additional_claims,
                                            expires_delta=timedelta(days=1))
         return jsonify(
             {'access_token': access_token, 'restaurant_id': restaurant.id, 'message': 'Login successful'}), 200
-    else:
-        return jsonify({'message': 'Invalid email or password'}), 401
+
+    # Sprawdź czy użytkownik to Employee
+    employee = Employee.query.filter_by(login=identifier).first()
+    if employee and check_password_hash(employee.password_hash, password):
+        additional_claims = {"employee_id": employee.employee_id}
+        access_token = create_access_token(identity=employee.employee_id, additional_claims=additional_claims,
+                                           expires_delta=timedelta(days=1))
+        return jsonify(
+            {'access_token': access_token, 'employee_id': employee.employee_id, 'message': 'Login successful'}), 200
+
+    return jsonify({'message': 'Niepoprawny login lub hasło'}), 401
